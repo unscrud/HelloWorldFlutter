2eb2f85cae14c7ce37b0e28d9fbc093e12bc7405
Daniel Carvalho <daniel.devop@gmail.com>
2020-07-22 20:15:15 -0300

    Basic implement of MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Center(
-      child: Text(
-        'Hello, World!',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 30.0),
+  runApp(MaterialApp(
+    title: 'Flutter Tutorial',
+    home: TutorialHome(),
+  ));
+}
+
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Menu de Navegação',
+              onPressed: null),
+          title: Text('Exemplo de Título'),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.search), tooltip: 'Buscar', onPressed: null)
+          ]),
+      body: Center(
+        child: Text('Olá mundo!'),
       ),
-    ),
-  );
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Adicionar',
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
+  }
 }
